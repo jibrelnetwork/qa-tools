fcd97d5b480a2e42c2360b7785ec5c3324a351b7
VAA <VasilyevAA>
2019-02-19 12:06:28 +0300

    add jsearch service structure

---
+++ JibrelTests/actions/common.py
@@ -0,0 +1,17 @@
+
+
+class ServiceCodes(object):
+    OK = 200
+    CREATED = 201
+    NO_CONTENT = 204
+    BAD_REQUEST = 400
+    NOT_FOUND = 404
+    SERVICE_ERROR = 500
+
+
+class Methods(object):
+    DELETE = "DELETE"
+    GET = "GET"
+    PATCH = "PATCH"
+    POST = "POST"
+    PUT = "PUT"
