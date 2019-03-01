7cfa877e3b08df58cf046134fb91c8d7e4797d94
VAA <VasilyevAA>
2019-03-01 19:34:19 +0300

    clear code

--- qa_tool/utils/api_helper.py
+++ qa_tool/utils/api_helper.py
@@ -7,7 +7,7 @@ from .utils import classproperty
 class Error(object):
     @classproperty
     def schema(cls):
-        return {'type': 'object', 'properties': {'field': {'type': 'string', 'example': 'NON_FIELD_ERROR'}, 'code': {'type': 'string', 'example': 'ERR_CODE'}, 'message': {'type': 'string', 'example': 'some err text'}}}
+        return {'type': 'object', 'properties': {'field': {'type': 'string'}, 'code': {'type': 'string'}, 'message': {'type': 'string'}}}
 
 
 class Errors(object):
@@ -20,12 +20,6 @@ class Status(object):
     @classproperty
     def schema(cls):
         return {'type': 'object', 'properties': {'success': {'type': 'boolean'}, 'errors': Errors.schema}}
-
-
-class Call(object):
-    @classproperty
-    def schema(cls):
-        return {'type': 'object', 'properties': {'from': {'type': 'string', 'required': False, 'description': 'The address the transaction is sent from.', 'example': '0x8aff0a12f3e8d55cc718d36f84e002c335df2f4a'}, 'to': {'type': 'string', 'required': False, 'description': 'The address the transaction is directed to.', 'example': '0x5c7687810ce3eae6cda44d0e6c896245cd4f97c6'}, 'gas': {'type': 'string', 'format': 'dec', 'required': False, 'description': 'Integer of the gas provided for the transaction execution.', 'example': '0'}, 'gasPrice': {'type': 'string', 'format': 'dec', 'required': False, 'description': 'Integer of the gas provided for the transaction execution.', 'example': '1'}, 'value': {'type': 'string', 'format': 'dec', 'required': False, 'description': 'Integer of the value sent with this transaction.', 'example': '0'}, 'data': {'type': 'string', 'description': 'Hash of the method signature and encoded parametelrs.', 'example': '0x6740d36c0000000000000000000000000000000000000000000000000000000000000005'}}}
 
 
 DEFAULT_ERROR = {
