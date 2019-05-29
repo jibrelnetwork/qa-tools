bbbbe655739d8b78748555557bad6be47867cab4
VAA <VasilyevAA>
2019-05-29 18:15:25 +0300

    **

---
+++ util_scripts/rc_merger.py
@@ -0,0 +1,11 @@
+
+# TODO: now we can get imeges from docker regestry need small research
+
+class TestMerge:
+
+    def test_collect_all_finished_tasks_for_each_service(self):
+        pass
+
+    def test_set_branch_name_for_repos_in_env_variables(self):
+        pass
+
