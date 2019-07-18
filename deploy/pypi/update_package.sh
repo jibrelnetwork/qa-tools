53df0b487564ede2118bff9351452a31db8e5ddb
VAA <VasilyevAA>
2019-07-18 13:43:20 +0300

    small refactor

--- deploy/pypi/update_package.sh
+++ deploy/pypi/update_package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-pip3 download -r /app/requirements.txt -d /srv/pypi
+pip3 download --no-cache-dir -r /app/requirements.txt -d /srv/pypi
 export PYTHONPATH=/app
 cd /app
 python3 setup.py sdist bdist_wheel -d /srv/pypi
