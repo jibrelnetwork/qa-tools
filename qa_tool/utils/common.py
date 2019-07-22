fb7265c26aeb8f9fb49dd56501644c2c73bc525c
VAA <VasilyevAA>
2019-07-22 18:29:19 +0300

    add more status codes

--- qa_tool/utils/common.py
+++ qa_tool/utils/common.py
@@ -14,8 +14,14 @@ class StatusCodes(object):
     CREATED = 201
     NO_CONTENT = 204
     BAD_REQUEST = 400
+    UNAUTHORIZED = 401
+    FORBIDDEN = 403
     NOT_FOUND = 404
+    CONFLICT = 409
+    UNPROCESSABLE_ENTITY = 422
+    TOO_MANY_REQUESTS = 429
     SERVICE_ERROR = 500
+    SERVICE_UNAVAILABLE = 503
 
 
 class Methods(object):
