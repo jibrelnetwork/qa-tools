e4bcf5f140d3ab2056e1aff3e00da54a6039d5a2
VAA <VasilyevAA>
2019-08-09 01:28:18 +0300

    add headers for all methods

--- qa_tool/custom_structure.py
+++ qa_tool/custom_structure.py
@@ -1,13 +1,22 @@
 import random
 
+from qa_tool.utils.utils import ClassPropertyDescriptor
+
+
+
 
 class Enum:
 
     @classmethod
     def as_dict(cls, matcher=None):
         matcher = matcher or (lambda value: True)
-        return {key: value for key, value in cls.__dict__.items() if
-                not key.startswith("_") and not isinstance(value, classmethod) and matcher(value)}
+        return {
+            key: value for key, value in cls.__dict__.items()
+            if not key.startswith("_")
+            and not isinstance(value, classmethod)
+            and matcher(value)
+            and not isinstance(value, ClassPropertyDescriptor)
+        }
 
     @classmethod
     def get_random(cls, matcher=None):
@@ -16,3 +25,19 @@ class Enum:
     @classmethod
     def get_all(cls, matcher=None):
         return sorted(cls.as_dict(matcher=matcher).values())
+
+
+if __name__ == '__main__':
+    from qa_tool.utils.utils import classproperty
+
+    class Keks(Enum):
+        qwe = 'qwe'
+        qweq = 'qweq'
+
+        @classproperty
+        def sad(self):
+            return 'asdxzc'
+
+    print(Keks.get_all())
+    qweqweqw = Keks.sad
+    print(qweqweqw)
