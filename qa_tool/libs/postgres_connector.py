28d42b9cced9cef692a58e4047755e0daa85c1ad
VAA <VasilyevAA>
2019-10-03 15:59:06 +0300

    more print in postgres and reporter.step

--- qa_tool/libs/postgres_connector.py
+++ qa_tool/libs/postgres_connector.py
@@ -26,7 +26,9 @@ class PostgresConnector(object):
         # TODO: Maybe RealDictCursor -> NamedTupleCursor https://stackoverflow.com/questions/6739355/dictcursor-doesnt-seem-to-work-under-psycopg2
         cursor_factory = psycopg2.extras.RealDictCursor if as_dict else psycopg2.extras.DictCursor
         with reporter.step("Postgre SQL to host: {host}:{port}, db_name: {database}".format(**self._connection_dict)):
-            logging.info("SQL: %s" % sql)
+            msg = "SQL: %s" % sql
+            logging.info(msg)
+            print(msg)
             with self.driver.connect(**self._connection_dict) as conn:
                 cursor = conn.cursor(cursor_factory=cursor_factory)
                 try:
@@ -34,6 +36,7 @@ class PostgresConnector(object):
                     return cursor.fetchall()
                 except (self.driver.ProgrammingError, self.driver.OperationalError) as e:
                     logging.error(e)
+            reporter.attach('DB Query', msg)
 
     def insert(self, table_name, dict_to_insert):
         """ WARNING! All strings passed unescaped """
