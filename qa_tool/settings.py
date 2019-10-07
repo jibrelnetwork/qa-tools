5a98a9b966185e992ef394f5b55b5bdc395fd140
VAA <VasilyevAA>
2019-10-07 14:31:08 +0300

    **

--- qa_tool/settings.py
+++ qa_tool/settings.py
@@ -4,8 +4,8 @@ from  distutils.util import strtobool
 JIRA_URL = "https://jibrelnetwork.atlassian.net/"
 JENKINS_JOB_BUILD_URL = os.getenv("BUILD_URL", '')
 ALLURE_PROJECT_ID = os.getenv("ALLURE_PROJECT_ID", 4)
-DISABLE_SCHEMA_VALIDATOR = strtobool(os.getenv("DISABLE_SCHEMA_VALIDATOR", False))
-IS_LOCAL_START = strtobool(os.getenv("IS_LOCAL_START", False))
+DISABLE_SCHEMA_VALIDATOR = strtobool(os.getenv("DISABLE_SCHEMA_VALIDATOR", 'False'))
+IS_LOCAL_START = strtobool(os.getenv("IS_LOCAL_START", 'False'))
 
 if DISABLE_SCHEMA_VALIDATOR:
     for _ in range(5):
