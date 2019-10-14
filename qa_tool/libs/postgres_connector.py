776bf37318a9f4ebd4fef9ca3b1b01e84c642baa
VAA <VasilyevAA>
2019-10-14 14:02:16 +0300

    support different isolation lvl in postgres

--- qa_tool/libs/postgres_connector.py
+++ qa_tool/libs/postgres_connector.py
@@ -5,6 +5,8 @@ from qa_tool.libs.reporter import reporter
 
 
 class PostgresConnector(object):
+    SERIALIZE = psycopg2.extensions.ISOLATION_LEVEL_SERIALIZABLE
+    AUTOCOMMIT = psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT
 
     def __init__(self, host, user, password, database=None):  # Do we need support async there?
         port = None
@@ -22,7 +24,7 @@ class PostgresConnector(object):
     def get_table_names(self):
         return [table["tablename"] for table in self.get("SELECT * FROM pg_catalog.pg_tables;")]
 
-    def get(self, sql, as_dict=True):
+    def get(self, sql, as_dict=True, isolate_lvl=None):
         # TODO: Maybe RealDictCursor -> NamedTupleCursor https://stackoverflow.com/questions/6739355/dictcursor-doesnt-seem-to-work-under-psycopg2
         cursor_factory = psycopg2.extras.RealDictCursor if as_dict else psycopg2.extras.DictCursor
         with reporter.step("Postgre SQL to host: {host}:{port}, db_name: {database}".format(**self._connection_dict)):
@@ -30,6 +32,7 @@ class PostgresConnector(object):
             logging.info(msg)
             print(msg)
             with self.driver.connect(**self._connection_dict) as conn:
+                conn.set_isolation_level(isolate_lvl or self.AUTOCOMMIT)
                 cursor = conn.cursor(cursor_factory=cursor_factory)
                 try:
                     cursor.execute(sql)
@@ -38,7 +41,7 @@ class PostgresConnector(object):
                     logging.error(e)
             reporter.attach('DB Query', msg)
 
-    def insert(self, table_name, dict_to_insert):
+    def insert(self, table_name, dict_to_insert, isolate_lvl=None):
         """ WARNING! All strings passed unescaped """
         columns = []
         values = []
@@ -51,8 +54,9 @@ class PostgresConnector(object):
             'table_name': table_name,
         }
         query = "INSERT INTO {table_name} ({columns}) VALUES ({values})".format(**query_data)
-        return self.get(query)
+        return self.get(query, isolate_lvl=isolate_lvl)
 
 
 if __name__ == "__main__":
-    print(PostgresConnector('127.0.0.1:5432', 'jticker', 'postgres', 'jticker').get_table_names())
+    print(PostgresConnector('127.0.0.1:9432', 'jassets', 'postgres', 'jassets').get_table_names())
+    print(PostgresConnector('127.0.0.1:9432', 'jassets', 'postgres', 'jassets').get('select * from assets;', isolate_lvl=PostgresConnector.SERIALIZE))
