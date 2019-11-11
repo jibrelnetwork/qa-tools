7dc8680e0a80686fb216713c9eb590098eab7624
VAA <VasilyevAA>
2019-11-11 15:00:55 +0300

    delete slack token

--- services/service_settings.py
+++ services/service_settings.py
@@ -4,7 +4,7 @@ from distutils.util import strtobool
 # SLACK_PORT = os.getenv("SLACK_PORT", '45455')
 SLACK_OBJ_TTL = int(os.getenv("SLACK_OBJ_TTL", 300))
 SLACK_TO_PORTAINER_HOOK_TIMEOUT = int(os.getenv("SLACK_TO_PORTAINER_HOOK_TIMEOUT", 60))
-SLACK_TOKEN = os.getenv("SLACK_TOKEN", 'xoxb-186470154240-813908724099-bK3asCsiM6XZ3Dc8TkFmJr3p')
+SLACK_TOKEN = os.getenv("SLACK_TOKEN", '')
 SLACK_FOLDER_CONFIG = os.getenv('SLACK_FOLDER_CONFIG', '.')
 
 PORTAINER_URL = os.getenv("PORTAINER_URL", 'https://portainer-dev.jdev.network')
