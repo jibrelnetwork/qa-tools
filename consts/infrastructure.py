1e822c5c312c11ffc029e009e42168df1ba028e6
VAA <VasilyevAA>
2020-02-21 18:34:20 +0300

    rename service scope add InfraServiceType edit Environment and add 'qa' and 'local'

--- consts/infrastructure.py
+++ consts/infrastructure.py
@@ -18,13 +18,18 @@ class InfrastructureUtil(Enum):
 
 
 class Environment(InfrastructureUtil):
-    DEV = 'Develop'
-    STAGE = 'Stage'
-    PROD = 'Production'
+    DEV = 'develop'
+    STAGE = 'stage'
+    PROD = 'production'
+    QA = 'qa'
 
     @classproperty
     def ALL(self):
-        return 'All'
+        return 'all'
+
+    @classproperty
+    def LOCAL(self):
+        return 'local'
 
     __env_alias = None
     @classproperty
@@ -43,8 +48,17 @@ class Environment(InfrastructureUtil):
 
 
 class ServiceScope(InfrastructureUtil):
-    JNA = 'JNA'
-    JIBRELCOM = 'Jibrelcom'
-    JTICKER = 'JTicker'
-    JSEARCH = 'JSearch'
-    COINMENA = 'Coinmena'
+    JNA = 'jna'
+    JIBRELCOM = 'jibrelcom'
+    JTICKER = 'jsicker'
+    JSEARCH = 'jsearch'
+    COINMENA = 'coinmena'
+
+
+class InfraServiceType:
+    PGBOUNCER = 'pgbouncer'
+    INFLUX_DB = 'influxdb'
+    KAFKA = 'kafka'
+    REDIS = 'redis'
+    ADMIN = 'admin'
+    CMS = 'default'
